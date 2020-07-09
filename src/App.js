e47d6c615da2524d6aa2cc90fbb2de419a7f0224
Farhan Farooq <farhan.farooq@live.com>
2020-07-09 20:49:11 +0500

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-      <h1>Shoe Store with React Router v6</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Container>
+        <h1>Shoe Store with React Router v6</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 
